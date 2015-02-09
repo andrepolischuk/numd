4281f4a0e4e092381a286360f7c12968c466c0b6
andrepolischuk <andre.polischuk@gmail.com>
2015-02-09 13:06:34 +0300

    Update test with minus & fractional

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('numd', function() {
 
     it('should return undefined', function() {
       assert.isUndefined(numd('rur', 'рубль', 'рубля', 'рублей'));
-      assert.isUndefined(numd({ 'usd': ['доллар', 'доллара', 'долларов']}));
+      assert.isUndefined(numd({ 'usd': ['доллар', 'доллара', 'долларов'] }));
     });
 
   });
@@ -21,13 +21,14 @@ describe('numd', function() {
       assert.isUndefined(numd.usd());
       assert.isUndefined(numd.gbp('abc'));
       assert.isUndefined(numd(22, 'марка'));
-      assert.isUndefined(numd.chf(2));
     });
 
     it('should return string', function() {
       assert.isString(numd(1, 'rur'));
       assert.isString(numd.usd(4));
       assert.isString(numd(7, 'франк', 'франка', 'франков'));
+      assert.isString(numd(150.2, 'метр', 'метра', 'метров'));
+      assert.isString(numd(-4, 'градус', 'градуса', 'градусов'));
     });
 
     it('should return `1 рубль`', function() {
@@ -40,6 +41,14 @@ describe('numd', function() {
 
     it('should return `7 франков`', function() {
       assert.equal('7 франков', numd(7, 'франк', 'франка', 'франков'));
+    });
+
+    it('should return `150.2 метра`', function() {
+      assert.equal('150.2 метра', numd(150.2, 'метр', 'метра', 'метров'));
+    });
+
+    it('should return `-4 градуса`', function() {
+      assert.equal('-4 градуса', numd(-4, 'градус', 'градуса', 'градусов'));
     });
 
   });
