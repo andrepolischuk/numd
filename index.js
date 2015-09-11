1c43ed3da334ad7acd8b6532e0fa56ce8cdab90b
andrepolischuk <andre.polischuk@gmail.com>
2015-09-11 23:37:29 +0300

    Tweaks

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-
 'use strict';
 
-/**
- * Expose pluralize
- *
- * @param  {Number|String} num
- * @param  {String} word
- * @param  {String} singular
- * @param  {String} plural
- * @return {String|Function}
- * @api public
- */
-
-module.exports = function(num, word, singular, plural) {
+module.exports = function (num, word, singular, plural) {
   return typeof num === 'number' ?
     pluralize(word, singular, plural)(num) :
     pluralize(num, word, singular);
 };
 
-/**
- * Pluralize value
- *
- * @param  {String} word
- * @param  {String} singular
- * @param  {String} plural
- * @return {Function}
- * @api private
- */
-
 function pluralize(word, singular, plural) {
   return function(num) {
     var abs = Math.abs(num);
     var str = num + ' ';
-
     if (num === 1) return str + word;
     if (!plural) return str + singular;
     if (isGenitivePlural(abs)) return str + plural;
@@ -41,27 +18,11 @@ function pluralize(word, singular, plural) {
   };
 }
 
-/**
- * Genetive plural test
- *
- * @param  {Number}  num
- * @return {Boolean}
- * @api private
- */
-
 function isGenitivePlural(num) {
   var nn = num % 10;
   return (num > 10 && ((num % 100) - ((num % 100) % 10)) / 10 === 1) ||
     nn === 0 || nn >= 5;
 }
-
-/**
- * Genetive singular test
- *
- * @param  {Number}  num
- * @return {Boolean}
- * @api private
- */
 
 function isGenitiveSingular(num) {
   return Math.floor(num) !== num || num % 10 >= 2;
--- test.js
+++ test.js
@@ -1,32 +1,26 @@
-
 'use strict';
-
 var numd = require('./');
 var assert = require('assert');
 
-describe('numd(num, ...words)', function() {
-  it('should return string', function() {
-    assert(numd(1, 'рубль', 'рубля', 'рублей') === '1 рубль');
-    assert(numd(4, 'доллар', 'доллара', 'долларов') === '4 доллара');
-    assert(numd(7, 'франк', 'франка', 'франков') === '7 франков');
-    assert(numd(150.2, 'метр', 'метра', 'метров') === '150.2 метра');
-    assert(numd(-4, 'градус', 'градуса', 'градусов') === '-4 градуса');
-    assert(numd(1, 'ruble', 'rubles') === '1 ruble');
-    assert(numd(4, 'dollar', 'dollars') === '4 dollars');
-    assert(numd(150.2, 'meter', 'meters') === '150.2 meters');
-    assert(numd(-4, 'degree', 'degrees') === '-4 degrees');
-  });
+it('should return string', function () {
+  assert(numd(1, 'рубль', 'рубля', 'рублей') === '1 рубль');
+  assert(numd(4, 'доллар', 'доллара', 'долларов') === '4 доллара');
+  assert(numd(7, 'франк', 'франка', 'франков') === '7 франков');
+  assert(numd(150.2, 'метр', 'метра', 'метров') === '150.2 метра');
+  assert(numd(-4, 'градус', 'градуса', 'градусов') === '-4 градуса');
+  assert(numd(1, 'ruble', 'rubles') === '1 ruble');
+  assert(numd(4, 'dollar', 'dollars') === '4 dollars');
+  assert(numd(150.2, 'meter', 'meters') === '150.2 meters');
+  assert(numd(-4, 'degree', 'degrees') === '-4 degrees');
 });
 
-describe('numd(...words)', function() {
-  it('should return function', function() {
-    var rurRu = numd('рубль', 'рубля', 'рублей');
-    assert(rurRu(1) === '1 рубль');
-    var degRu = numd('градус', 'градуса', 'градусов');
-    assert(degRu(-4) === '-4 градуса');
-    var rurEn = numd('ruble', 'rubles');
-    assert(rurEn(1) === '1 ruble');
-    var degEn = numd('degree', 'degrees');
-    assert(degEn(-4) === '-4 degrees');
-  });
+it('should return function', function () {
+  var rurRu = numd('рубль', 'рубля', 'рублей');
+  var degRu = numd('градус', 'градуса', 'градусов');
+  var rurEn = numd('ruble', 'rubles');
+  var degEn = numd('degree', 'degrees');
+  assert(rurRu(1) === '1 рубль');
+  assert(degRu(-4) === '-4 градуса');
+  assert(rurEn(1) === '1 ruble');
+  assert(degEn(-4) === '-4 degrees');
 });
