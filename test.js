367706534f9efaffe3ba70f40eeccfd32be99f59
andrepolischuk <andre.polischuk@gmail.com>
2016-01-06 22:25:36 +0300

    Add ava instead of mocha

--- test.js
+++ test.js
@@ -1,26 +1,22 @@
 'use strict';
+var test = require('ava');
 var numd = require('./');
-var assert = require('assert');
 
-it('should return string', function () {
-  assert(numd(1, 'рубль', 'рубля', 'рублей') === '1 рубль');
-  assert(numd(4, 'доллар', 'доллара', 'долларов') === '4 доллара');
-  assert(numd(7, 'франк', 'франка', 'франков') === '7 франков');
-  assert(numd(150.2, 'метр', 'метра', 'метров') === '150.2 метра');
-  assert(numd(-4, 'градус', 'градуса', 'градусов') === '-4 градуса');
-  assert(numd(1, 'ruble', 'rubles') === '1 ruble');
-  assert(numd(4, 'dollar', 'dollars') === '4 dollars');
-  assert(numd(150.2, 'meter', 'meters') === '150.2 meters');
-  assert(numd(-4, 'degree', 'degrees') === '-4 degrees');
+test('return string', function (t) {
+  t.is(numd(1, 'рубль', 'рубля', 'рублей'), '1 рубль');
+  t.is(numd(4, 'доллар', 'доллара', 'долларов'), '4 доллара');
+  t.is(numd(7, 'франк', 'франка', 'франков'), '7 франков');
+  t.is(numd(150.2, 'метр', 'метра', 'метров'), '150.2 метра');
+  t.is(numd(-4, 'градус', 'градуса', 'градусов'), '-4 градуса');
+  t.is(numd(1, 'ruble', 'rubles'), '1 ruble');
+  t.is(numd(4, 'dollar', 'dollars'), '4 dollars');
+  t.is(numd(150.2, 'meter', 'meters'), '150.2 meters');
+  t.is(numd(-4, 'degree', 'degrees'), '-4 degrees');
 });
 
-it('should return function', function () {
-  var rurRu = numd('рубль', 'рубля', 'рублей');
-  var degRu = numd('градус', 'градуса', 'градусов');
-  var rurEn = numd('ruble', 'rubles');
-  var degEn = numd('degree', 'degrees');
-  assert(rurRu(1) === '1 рубль');
-  assert(degRu(-4) === '-4 градуса');
-  assert(rurEn(1) === '1 ruble');
-  assert(degEn(-4) === '-4 degrees');
+test('return function', function (t) {
+  t.is(numd('рубль', 'рубля', 'рублей')(1), '1 рубль');
+  t.is(numd('градус', 'градуса', 'градусов')(-4), '-4 градуса');
+  t.is(numd('ruble', 'rubles')(1), '1 ruble');
+  t.is(numd('degree', 'degrees')(-4), '-4 degrees');
 });
